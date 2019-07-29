3565200cac454045d6794257be7995ab764a6b8f
origrim <lagorn@ro.ru>
2019-07-29 11:06:51 +0400

    Revert "remout"
    This reverts commit 013e9323567dff6f127fabb59eff30b92f34af90.
    
    # Conflicts:
    #	src/DateService.java
    #	src/DemoService.java

--- src/DateService.java
+++ src/DateService.java
@@ -1,38 +1,18 @@
+import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.Locale;
+import java.util.logging.SimpleFormatter;
 
 public class DateService {
+    public void youAge (/*LocalDateTime myBirthday*/) {
+        Date timenow = new Date();
 
-    public void youAge(LocalDateTime birthTime) {
+        SimpleDateFormat formatForDate = new SimpleDateFormat("'Эра 'G dd-MM-yyyy 'и время: ' HH:mm:ss:SSS");
 
-        LocalDateTime currentTime = LocalDateTime.now();
+        System.out.println(formatForDate.format(timenow));
 
-        System.out.printf(Locale.US, "С момента рождения прошло: %d лет, %d месяцев, %,d дней, %,d часов, %,d минут, %,d секунд\n",
-                ChronoUnit.YEARS.between(birthTime, currentTime),
-                ChronoUnit.MONTHS.between(birthTime, currentTime),
-                ChronoUnit.DAYS.between(birthTime, currentTime),
-                ChronoUnit.HOURS.between(birthTime, currentTime),
-                ChronoUnit.MINUTES.between(birthTime, currentTime),
-                ChronoUnit.SECONDS.between(birthTime, currentTime)
-        );
-    }
-
-    public long daysBetween(String firstDateStr, String secondDateStr) {
-
-        DateTimeFormatter format =  DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        LocalDate firstDate = LocalDate.parse(firstDateStr, format);
-        LocalDate secondDate = LocalDate.parse(secondDateStr, format);
-
-        return  ChronoUnit.DAYS.between(firstDate, secondDate);
-    }
-
-    public String convertDate (String dateStr) {
-
-        ZonedDateTime date = ZonedDateTime.parse(dateStr).withZoneSameInstant(ZoneId.of("Europe/Samara"));
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-        return date.format(formatter);
     }
 }
--- src/DemoService.java
+++ src/DemoService.java
@@ -1,63 +1,21 @@
-import java.text.DecimalFormat;
-import java.time.LocalDateTime;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class DemoService {
+    public void HowOldIam(){
+        LocalDate timenow = LocalDate.now();
+        System.out.println(timenow);
 
-    public void execute() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startDate = LocalDate.parse("01.07.1993", formatter);
+        LocalDate endDate = LocalDate.parse("05.03.2013", formatter);
+        Period period = Period.between(startDate, timenow);
+        System.out.println("Прошло " + period.getYears()+ " года.");
+        System.out.println("Прошло " + period.getMonths()+ " месяца.");
+        System.out.println("Прошло " + period.getDays()+ " дня.");
 
-        //Date
-        System.out.println("Даты:");
-
-        DateService dateService = new DateService();
-
-        //1.1
-        System.out.print("1.1 ");
-        dateService.youAge(LocalDateTime.of(1993, 7, 1, 11, 45, 0));
-
-        //1.2
-        String str1 = "25.07.2007";
-        String str2 = "25.07.2018";
-        System.out.printf("1.2 Между %s и %s прошло %d дней\n",
-                str1,
-                str2,
-                dateService.daysBetween(str1, str2)
-        );
-
-        //1.3
-        str1 = "2016-08-16T23:11:26-11:00";
-        System.out.printf("1.3 Конвертация \"%s\" в Ижевское время \"%s\"\n\n",
-                str1,
-                dateService.convertDate(str1)
-        );
-
-        //Other
-        System.out.println("Прочее:");
-
-        NumberService numberService = new NumberService();
-
-        //2.1
-        double d1 = 6.7;
-        System.out.printf("2.1 Площадь круга с радиусом %s равна %s\n",
-                new DecimalFormat("0.#######").format(d1),
-                numberService.circleSquare(d1).toString()
-        );
-
-        //2.2
-        str1 = "0.1";
-        str2 = "0.15";
-        String str3 = "0.25";
-        System.out.printf("2.2 Даны три числа: %s, %s и %s. Третье число%sявляется суммой первых двух\n",
-                str1, str2, str3,
-                numberService.isThirdSumOfFirstTwo(str1, str2, str3) ? " " : " НЕ ");
-
-        //2.3
-        int i1 = 4;
-        int i2 = 35;
-        int i3 = -10;
-        System.out.printf("2.3 Даны три числа: %d, %d и %d. Минимальное: %d Максимальное: %d\n\n",
-                i1, i2, i3,
-                numberService.findMin(i1, i2, i3),
-                numberService.findMax(i1, i2, i3)
-        );
+        LocalDateTime timenow_new = LocalDateTime.MIN.now();
+        System.out.println(timenow_new);
     }
+
 }
