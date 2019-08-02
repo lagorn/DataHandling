f32de1554c3a439087480a9f491d924f8727c714
lagorn <lagorn@ro.ru>
2019-08-02 23:49:26 +0400

    task done 1.2 v2

--- src/DateService.java
+++ src/DateService.java
@@ -2,12 +2,10 @@ import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.Date;
 import java.util.Locale;
 import java.util.logging.SimpleFormatter;
 
 public class DateService {
-
     public void youAge(LocalDateTime myBirthday){
         System.out.println(myBirthday);
         LocalDateTime dateToday = LocalDateTime.now();
